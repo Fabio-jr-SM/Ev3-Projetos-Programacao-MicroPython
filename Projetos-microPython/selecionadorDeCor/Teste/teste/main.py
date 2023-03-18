ead2b9ba0e7dd39ab0aa74f04001ee6c72fcb11f
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-03-18 19:54:41 -0400

    Update main.py

--- Projetos-microPython/selecionadorDeCor/Teste/teste/main.py
+++ Projetos-microPython/selecionadorDeCor/Teste/teste/main.py
@@ -1,20 +1,29 @@
-#!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
+# Inicializa o EV3 Brick.
 ev3 = EV3Brick()
 
+# Inicializa os motores.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
 
-# Write your program here.
+# Inicializa o drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Anda a uma distancia de 1000mm e depois retorna.
+robot.straight(1000)
+ev3.speaker.beep()
+
+robot.straight(-1000)
+ev3.speaker.beep()
+
+# Gira em um angulo de 360 graus.
+robot.turn(360)
+ev3.speaker.beep()
+
+robot.turn(-360)
 ev3.speaker.beep()
