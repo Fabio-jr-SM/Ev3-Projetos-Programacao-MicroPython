4a4b9a13e2ac410aa6cbb2456b3be050d8bcb3b4
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-06-09 21:44:20 -0400

    Update MovBasic.py

--- Projetos-microPython/MovimentacaoBasicaa/MovBasic.py
+++ Projetos-microPython/MovimentacaoBasicaa/MovBasic.py
@@ -12,25 +12,23 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-
 # Inicializa o drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 
+
+#**********************************************************************
 # Anda a uma distancia de 1000mm e depois retorna.
 robot.straight(1000)
 ev3.speaker.beep()
-
-
 robot.straight(-1000)
 ev3.speaker.beep()
-
 
 # Gira em um angulo de 360 graus.
 robot.turn(360)
 ev3.speaker.beep()
-
-
 robot.turn(-360)
 ev3.speaker.beep()
+
+#**********************************************************************
 
